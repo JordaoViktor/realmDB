e2a3566f5a831488c5aeaa7e3d4acfbff3413a74
JordaoViktor <jordaovpinheiro@gmail.com>
2021-06-07 01:05:50 -0300

    feat: add status bar

--- App.js
+++ App.js
@@ -7,10 +7,20 @@
  */
 
 import React from 'react';
+import {StatusBar} from 'react-native';
 import Main from './src/screens/Main';
 
 const App = () => {
-  return <Main />;
+  return (
+    <>
+      <StatusBar
+        backgroundColor="transparent"
+        translucent
+        barStyle="light-content"
+      />
+      <Main />
+    </>
+  );
 };
 
 export default App;
